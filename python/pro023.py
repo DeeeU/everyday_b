5a01f94ecdfca59e362dd3b6b0041d189b46ed3d
Muragaruhae <jac8for@gmail.com>
2020-03-22 16:55:22 +0900

    change code

--- python/pro023.py
+++ python/pro023.py
@@ -1,19 +1,18 @@
 n = int(input())
 s = input()
-count = (6-1)//2
-print(count)
-# acs = "b"
-# i = 0
-# while i < count:
-#     if i % 3 == 0:
-#         acs = "a" + acs + "c"
-#     elif i % 3 == 1:
-#         acs = "c" + acs + "a"
-#     else:
-#         acs = "b" + acs + "b"
-#     i += 1
+count = (n-1)//2
+acs = "b"
+i = 0
+while i < count:
+    if i % 3 == 0:
+        acs = "a" + acs + "c"
+    elif i % 3 == 1:
+        acs = "c" + acs + "a"
+    else:
+        acs = "b" + acs + "b"
+    i += 1
 
-# if s == acs:
-#     print(count)
-# else:
-#     print(-1)
+if s == acs:
+    print(count)
+else:
+    print(-1)
