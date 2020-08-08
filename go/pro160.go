ed5dc126711c8734041edf524e754ca753d2a34a
Muragaruhae <jac8for@gmail.com>
2020-08-08 14:48:40 +0900

    update

---
+++ go/pro160.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	x := nextInt()
+	acount := x / 500
+	bcount := x % 500 / 5
+	fmt.Println(acount*1000 + bcount*5)
+
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
