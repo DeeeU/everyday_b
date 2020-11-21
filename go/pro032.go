be1204d584f59807d6a57e2167e01d05992f2606
Muragaruhae <jac8for@gmail.com>
2020-11-22 00:36:56 +0900

    update

---
+++ go/pro032.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	var (
+		s string
+		k int
+	)
+	s, k = nextStr(), nextInt()
+	m := make(map[string]bool)
+
+	for i := 0; i < len(s)-k+1; i++ {
+		m[s[i:i+k]] = true
+	}
+	fmt.Println(len(m))
+}
+
+func init() {
+	const max = 1024
+	var buf = make([]byte, max)
+	sc.Buffer(buf, max)
+	sc.Split(bufio.ScanWords)
+	return
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nextStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// func max(a int, b int) int {
+// 	if a > b {
+// 		return a
+// 	} else {
+// 		return b
+// 	}
+// }
