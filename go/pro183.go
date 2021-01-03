9ce0561be9b962d8a67a0047d319f93488bd38c2
Muragaruhae <jac8for@gmail.com>
2021-01-03 23:37:30 +0900

    update

---
+++ go/pro183.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	var sx, sy, gx, gy float64
+	sx, sy, gx, gy = nextFloat64(), nextFloat64(), nextFloat64(), nextFloat64()
+	fmt.Println((sx*gy + sy*gx) / (sy + gy))
+}
+
+func init() {
+	const max = 1024
+	var buf = make([]byte, max)
+	sc.Buffer(buf, max)
+	sc.Split(bufio.ScanWords)
+	return
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nextStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+func nextFloat64() float64 {
+	f, err := strconv.ParseFloat(nextStr(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// func max(a int, b int) int {
+// 	if a > b {
+// 		return a
+// 	} else {
+// 		return b
+// 	}
+// }
